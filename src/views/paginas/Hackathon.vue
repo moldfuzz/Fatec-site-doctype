cdbce2075ecbb3f79c2f5ea0928c14d4c3eb91dc
Aline <aline.isilvadamiao@gmail.com>
2019-10-17 15:27:50 -0300

    Update Hackathon.vue

--- src/views/paginas/Hackathon.vue
+++ src/views/paginas/Hackathon.vue
@@ -1,100 +1,29 @@
 <template>
-  
-  
-<body>
-  
-<div class="login-box">
-  <h1>Login</h1>
-  <div class="textbox">
-    <i class="fas fa-user"></i>
-    <input type="text" placeholder="Username">
-  </div>
+  <div>
+   
+  <body>
+    <h1>Plunct! Plact! Zum! Você não vai a Hackathon algum sem se inscrever!</h1>
+ <section class="informacao-hackathon white--text">
+ 
+  <v-stepper vertical class="transparent " >
+    <v-stepper-step step="1" complete>Name of step 1 </v-stepper-step> 
+    <v-stepper-content step="1"> </v-stepper-content>
+    <v-stepper-step step="2 white--text" complete>Name of step 2</v-stepper-step>
+     <v-stepper-content step="2"> </v-stepper-content>
+    <v-stepper-step step="3">Name of step 2</v-stepper-step>
 
-  <div class="textbox">
-    <i class="fas fa-lock"></i>
-    <input type="email" placeholder="e-mail">
-  </div>
+  </v-stepper>
 
-  <div class="textbox">
-    <input type="date" >
-  </div>
+</section>    
+        
+  </body>
 
-  <div class="textbox">
-    <input type="text" placeholder="Faculdade">
   </div>
-
-  <div class="textbox">
-    <input type="text" placeholder="Curso">
-  </div>
-
-  <div class="textbox">
-    <input type="text" placeholder="Endereço">
-  </div>
-
-  <input type="button" class="btn" value="Sign in">
-</div>
-</body>       
-  
-  
 </template>
-<style scoped>
 
-body{
-  margin: 0;
-  padding: 0;
-  font-family: sans-serif;
-  background-color: blue;
-  /* background-image: url(/assets/foguete-hackathon.jpg) no-repeat; */
-  background-size: cover;
-}
-.login-box{
-  width: 280px;
-  position: relative;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%,-50%);
-  color: white;
-  background-color: rgba(0, 0, 0, 0.7);
-}
-.login-box h1{
-  text-align: center;
-  font-size: 40px;
-  border-bottom: 6px solid #4caf50;
-  margin-bottom: 50px;
-  padding: 13px 0;
-}
-.textbox{
-  width: 100%;
-  overflow: hidden;
-  font-size: 20px;
-  padding: 8px 0;
-  margin: 8px 0;
-  border-bottom: 1px solid #4caf50;
-}
-.textbox i{
-  width: 26px;
-  float: left;
-  text-align: center;
-}
-.textbox input{
-  border: none;
-  outline: none;
-  background: none;
-  color: white;
-  font-size: 18px;
-  width: 80%;
-  float: left;
-  margin: 0 10px;
-}
-.btn{
-  width: 100%;
-  background: none;
-  border: 2px solid #4caf50;
-  color: white;
-  padding: 5px;
-  font-size: 18px;
-  cursor: pointer;
-  margin: 12px 0;
-}
+<script>
+
+</script>
+<style scoped>
 
 </style>
