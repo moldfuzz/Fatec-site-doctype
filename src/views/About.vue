4465db6ddd94c75aeae842e6781f51a077e096a5
Aline <aline.isilvadamiao@gmail.com>
2019-10-17 20:42:22 -0300

    pagina sobre
    pagina sobre

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <body>
+      <section class="sobre-section">
+        <h3>{Sobre nós}</h3>
+        <hr class="border">
+        <div class="sobre-section-col">
+          <div class="sobre">
+            <p>
+              É importante lembrar de onde viemos para saber aonde queremos chegar. Antes de nos unirmos para formar uma das maiores empresas de desenvolvimento na área da tecnologia, tivemos uma história cheia de desafios e de conquistas que nos trouxeram até aqui. 
+              Conheça os marcos mais importantes da nossa história!
+            </p>
+        
+          </div>
+        </div>
+        <div class="sobre-section-row">
+          <div class="sobre-section-col">
+          <div class="video">
+            <h3 class="texto">
+
+            </h3>
+          </div>
+        </div>
+        </div>
+        
+      </section>
+    </body> 
   </div>
 </template>
